3c3c70226d8095d16f50b69bb8f0a7f1da354075
Veshal Raj <veshalraj1307@gmail.com>
2023-06-07 13:38:50 +0530

    fibanacci-firt-commit-Array

---
+++ Fibanacci.js
@@ -0,0 +1,19 @@
+function fibonacci(n){
+    const fib = [0,1]
+    if(n==0){
+        return "Undefined"
+    }else if(n==1){
+        return 0;
+    }
+    else{
+
+        for(let i=2; i<n; i++){
+            fib[i]=fib[i-1]+fib[i-2]
+        }
+    }
+    return fib
+}
+
+console.log(fibonacci(2)) //[0,1]
+console.log(fibonacci(3)) //[0,1,1]
+console.log(fibonacci(7)) //[0,1,1,2,3,5,8]
