b2453b875f37c764cccec68ff3f6a4cd40d77c71
Veshal Raj <veshalraj1307@gmail.com>
2023-06-09 17:13:13 +0530

    linkedList-demo1

--- linearSearch-.js
+++ linearSearch-.js
@@ -7,5 +7,5 @@ function linearSearch(arr,target){
     return -1;
 }
 console.log(linearSearch([-8,2,3,5,1,6],5))
-console.log(linearSearch([-8,2,3,5,1,6],1))
+console.log(linearSearch([-8,2,3,5,1,6],6))
 console.log(linearSearch([-8,2,3,5,1,6],4))
