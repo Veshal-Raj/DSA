b3d436a63829d84f37521ab51525940ff2747a0a
Veshal Raj <veshalraj1307@gmail.com>
2023-06-11 19:54:20 +0530

    basicNotes

--- linkedList-class.js
+++ linkedList-class.js
@@ -1,23 +1,25 @@
-
+//node is a single piece of linked list
 class Node {
     constructor(value){
         this.value = value
         this.next = null
     }
 }
+
+//linkedList is a collection of nodes
 class LinkedList{
     constructor(){
-        this.head = null
-        this.size = 5
+        this.head = null //reference to first value, for eg: { (A)-->null }
+        this.size = 0  // no.of nodes in linkedlist (default -0)
     }
     isEmpty(){
-        return this.size===5
+        return this.size===0
     }
     getSize(){
         return this.size
     }
 }
 
-const list = new LinkedList()
+const list = new LinkedList()  //object
 console.log('List is empty?',list.isEmpty())
 console.log('List size',list.getSize())
