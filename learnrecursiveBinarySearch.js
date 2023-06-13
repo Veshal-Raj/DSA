8d6c7e3b92b96e40e5b00ef2de7b879f2a818da1
Veshal Raj <veshalraj1307@gmail.com>
2023-06-13 18:38:18 +0530

    recursivebinarySearch

---
+++ learnrecursiveBinarySearch.js
@@ -0,0 +1,19 @@
+function recursiveBinarySearch(arr,target){
+    return Search(arr,target,0,arr.length-1)
+}
+function Search(arr,target,leftIndex,rightIndex){
+    if(leftIndex>rightIndex){
+        return -1
+    }
+    let middleIndex = Math.floor((leftIndex+rightIndex)/2)
+    if(target===arr[middleIndex]){
+        return middleIndex
+    }
+    if(target<arr[middleIndex]){
+        return Search(arr,target,leftIndex,middleIndex-1)
+    }else{
+        return Search(arr,target,middleIndex+1,rightIndex)
+    }
+}
+
+console.log(recursiveBinarySearch([1,2,3,4],2))
